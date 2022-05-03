444587417399e5dfe758a2a1937172d5420ae99c
Ilya Volobuev <64730225+Epicwater003@users.noreply.github.com>
2022-05-04 00:12:11 +0300

    Setters/Getters added

--- Circle.h
+++ Circle.h
@@ -42,6 +42,28 @@ public:
     void Update();
     Mesh CreateMesh() { return CreateCircleMesh(resolution); }
     /*========================================*/
+    glm::vec2 GetPosition()        { return BodyContract->GetPosition()           ; }
+    glm::vec2 GetDirection()       { return BodyContract->GetDirection()          ; }
+    glm::vec2 GetVelocity()        { return BodyContract->GetVelocity()           ; }
+    glm::vec2 GetAcceleration()    { return BodyContract->GetAcceleration()       ; }
+    glm::vec2 GetMassCenter()      { return BodyContract->GetMassCenter()         ; }
+    float GetDensity()             { return BodyContract->GetDensity()            ; }
+    float GetMass()                { return BodyContract->GetMass()               ; }
+    float GetAngle()               { return BodyContract->GetAngle()              ; }
+    float GetAngularVelocity()     { return BodyContract->GetAngularVelocity()    ; }
+    float GetAngularAcceleration() { return BodyContract->GetAngularAcceleration(); }
+
+    void SetPosition(glm::vec2 p)        { BodyContract->SetPosition(p)           ; }
+    void SetDirection(glm::vec2 d)       { BodyContract->SetDirection(d)          ; }
+    void SetVelocity(glm::vec2 v)        { BodyContract->SetVelocity(v)           ; }
+    void SetAcceleration(glm::vec2 a)    { BodyContract->SetAcceleration(a)       ; }
+    void SetMassCenter(glm::vec2 mc)     { BodyContract->SetMassCenter(mc)        ; }
+    void SetDensity(float d)             { BodyContract->SetDensity(d)            ; }
+    void SetMass(float m)                { BodyContract->SetMass(m)               ; }
+    void SetAngle(float a)               { BodyContract->SetAngle(a)              ; }
+    void SetAngularVelocity(float w)     { BodyContract->SetAngularVelocity(w)    ; }
+    void SetAngularAcceleration(float e) { BodyContract->SetAngularAcceleration(e); }
+
     void Move(glm::vec2 position) {
         BodyContract->Move(position);
     }
