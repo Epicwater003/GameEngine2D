59231411a9647f5051cb5b6a5db028f40cbc6510
Ilya Volobuev <64730225+Epicwater003@users.noreply.github.com>
2022-05-04 00:03:24 +0300

    Getters/Setters added

--- IBody2D.h
+++ IBody2D.h
@@ -7,6 +7,30 @@ class IBody2D
 {
 public:
 	// TODO: Явно задать контракт на геттеры и сеттеры
+	
+
+	virtual glm::vec2 GetPosition() = 0;
+	virtual glm::vec2 GetDirection() = 0;
+	virtual glm::vec2 GetVelocity() = 0;
+	virtual glm::vec2 GetAcceleration() = 0;
+	virtual glm::vec2 GetMassCenter() = 0;
+	virtual float GetDensity() = 0;
+	virtual float GetMass() = 0;
+	virtual float GetAngle() = 0;
+	virtual float GetAngularVelocity() = 0;
+	virtual float GetAngularAcceleration() = 0;
+
+	virtual void SetPosition(glm::vec2) = 0;
+	virtual void SetDirection(glm::vec2) = 0;
+	virtual void SetVelocity(glm::vec2) = 0;
+	virtual void SetAcceleration(glm::vec2) = 0;
+	virtual void SetMassCenter(glm::vec2) = 0;
+	virtual void SetDensity(float) = 0;
+	virtual void SetMass(float) = 0;
+	virtual void SetAngle(float) = 0;
+	virtual void SetAngularVelocity(float) = 0;
+	virtual void SetAngularAcceleration(float) = 0;
+
 	virtual void Move(glm::vec2 position)   = 0;		   // Перемещение в соответствии с вектором скорости и временем кадра
 	virtual void Rotate(float angle)        = 0;  		   // Поворот относительно центра масс по времени
 	virtual glm::vec2 CalculateMassCenter() = 0;           // Расчет центра масс
--- RigidBody2D.h
+++ RigidBody2D.h
@@ -8,6 +8,28 @@
 class RigidBody2D: 
 	public IBody2D 
 {
+	glm::vec2 GetPosition()        { return position           ; }
+	glm::vec2 GetDirection()       { return direction          ; }
+	glm::vec2 GetVelocity()        { return velocity           ; }
+	glm::vec2 GetAcceleration()    { return acceleration       ; }
+	glm::vec2 GetMassCenter()      { return massCenter         ; }
+	float GetDensity()             { return density            ; }
+	float GetMass()                { return mass               ; }
+	float GetAngle()               { return angle              ; }
+	float GetAngularVelocity()     { return angularVelocity    ; }
+	float GetAngularAcceleration() { return angularAcceleration; }
+
+	void SetPosition(glm::vec2 p)        { position = p           ; }
+	void SetDirection(glm::vec2 d)       { direction = d          ; }
+	void SetVelocity(glm::vec2 v)        { velocity = v           ; }
+	void SetAcceleration(glm::vec2 a)    { acceleration = a       ; }
+	void SetMassCenter(glm::vec2 mc)     { massCenter = mc        ; }
+	void SetDensity(float d)             { density = d            ; }
+	void SetMass(float m)                { mass = m               ; }
+	void SetAngle(float a)               { angle = a              ; }
+	void SetAngularVelocity(float w)     { angularVelocity = w    ; }
+	void SetAngularAcceleration(float e) { angularAcceleration = e; }
+
 	glm::vec2 position     = { 0., 0. }; // Позиция
 	glm::vec2 direction    = { 0., 1. }; // Вектор направления(лицо объекта)
 	glm::vec2 velocity     = { 0., 0. }; // Скорость
@@ -21,7 +43,7 @@ class RigidBody2D:
 protected:
 	void Move(glm::vec2 position)          ;         // Перемещение в соответствии с вектором скорости и временем кадра
 	void Rotate(float angle)               ;         // Поворот относительно центра масс по времени
-	virtual glm::vec2 CalculateMassCenter();         // Расчет центра масс
+	glm::vec2 CalculateMassCenter();         // Расчет центра масс
     glm::vec2 CalculateMassCenter(Mesh m);           // Универсальный расчет центра масс в зависимости от меша
 };
 
